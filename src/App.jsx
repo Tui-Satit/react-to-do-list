0df443b0455fe769c47dbb6359d737fcfb65f83d
Tui <m.satit@gmail.com>
2026-07-01 12:07:18 +0700

    Improve Calendar

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,11 @@ function App() {
   const [openTasks, setOpenTasks] = useState([]);
 
   function formatDate(date) {
-    return date.toISOString().split("T")[0];
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
   }
 
   const selectedDateText = formatDate(selectedDate);
